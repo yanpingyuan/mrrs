89ae3e63d504ed73846dacb3ab83195e14da50b7
Daniel <yanpingyuan@shinetechsoftware.com>
2025-01-27 17:00:38 +0800

    add mui

--- mrrm-web/src/app/layout.tsx
+++ mrrm-web/src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {AppRouterCacheProvider} from '@mui/material-nextjs/v15-appRouter';
+import { StyledRoot } from './styledRoot';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
+      <body >
+        <AppRouterCacheProvider>
+          <StyledRoot>{children}</StyledRoot>
+        </AppRouterCacheProvider>
       </body>
+       
+      {/* </body> */}
     </html>
   );
 }
